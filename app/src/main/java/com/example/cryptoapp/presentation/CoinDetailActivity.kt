d7b75d07e1d90acf291b752f01d248c0072a6ecb
sergey <sergey066@yandex.ru>
2023-05-06 18:09:04 +0300

    CoinDetailFragment added

--- app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
@@ -5,13 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +19,11 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this)  {
-            with(binding) {
-                tvPrice.text = it.price
-                tvMinPrice.text = it.lowDay
-                tvMaxPrice.text = it.highDay
-                tvLastMarket.text = it.lastMarket
-                tvLastUpdate.text = it.lastUpdate
-                tvFromSymbol.text = it.fromSymbol
-                tvToSymbol.text = it.toSymbol
-                Picasso.get().load(it.imageUrl).into(ivLogoCoin)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 
--- app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -3,6 +3,8 @@ package com.example.cryptoapp.presentation.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil.ItemCallback
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ItemCoinInfoBinding
@@ -10,26 +12,19 @@ import com.example.cryptoapp.domain.CoinInfo
 import com.squareup.picasso.Picasso
 
 class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
-
-    var coinInfoList: List<CoinInfo> = listOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+    ListAdapter<CoinInfo, CoinInfoAdapter.CoinInfoViewHolder>(CoinInfoDiffCallback) {
 
     var onCoinClickListener: OnCoinClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
-        val view =
+        return CoinInfoViewHolder(
             ItemCoinInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CoinInfoViewHolder(view)
+        )
     }
 
-    override fun getItemCount() = coinInfoList.size
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        val coin = coinInfoList[position]
+        val coin = getItem(position)
         with(holder.binding) {
             with(coin) {
                 val symbolsTemplate = context.resources.getString(R.string.symbols_template)
@@ -46,9 +41,21 @@ class CoinInfoAdapter(private val context: Context) :
         }
     }
 
-    inner class CoinInfoViewHolder(val binding: ItemCoinInfoBinding) : RecyclerView.ViewHolder(binding.root)
+    class CoinInfoViewHolder(val binding: ItemCoinInfoBinding)
+        : RecyclerView.ViewHolder(binding.root)
 
     interface OnCoinClickListener {
-        fun onCoinClick(coinInfoDto: CoinInfo)
+        fun onCoinClick(coinInfo: CoinInfo)
+    }
+
+    object CoinInfoDiffCallback : ItemCallback<CoinInfo>() {
+        override fun areItemsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
+            return oldItem.fromSymbol == newItem.fromSymbol
+        }
+
+        override fun areContentsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
+            return oldItem == newItem
+        }
+
     }
 }
